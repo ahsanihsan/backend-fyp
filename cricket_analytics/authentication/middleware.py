e752486e78170f8c55f6b787e94b98609f6a3573
Ahsan Ihsan <ahsan.ihsan@outlook.com>
2020-11-17 04:02:18 +0500

    redasfsd

--- cricket_analytics/authentication/middleware.py
+++ cricket_analytics/authentication/middleware.py
@@ -1,9 +1,9 @@
-from datetime import datetime
-from jose import jwt
-from django.contrib.auth.models import User
+# from datetime import datetime
+# from jose import jwt
+# from django.contrib.auth.models import User
 
-class Authenticate(object):
-    def process_request(self, request):
-        if 'HTTP_AUTHORIZATION' in request.META:
-            token =request.META['HTTP_AUTHORIZATION']
-            request.current_user = User.objects.get(id = jwt.decode(token, 'seKre8')['id'])
+# class Authenticate(object):
+#     def process_request(self, request):
+#         if 'HTTP_AUTHORIZATION' in request.META:
+#             token =request.META['HTTP_AUTHORIZATION']
+#             request.current_user = User.objects.get(id = jwt.decode(token, 'seKre8')['id'])
